1f755134997cef61556c321de0fb8ebe8b0d78cb
erynalexa <eryn.dickison@gmail.com>
2017-03-21 21:27:24 -0400

    changed model.py to HeatExhaustion instead of Heatexhaustion

--- hotORnot/alerts/admin.py
+++ hotORnot/alerts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import HeatExhaustion
-
-admin.site.register(HeatExhaustion)
+from models import HeatExhaustion
 
 # Register your models here.
+
+admin.site.register(HeatExhaustion)
